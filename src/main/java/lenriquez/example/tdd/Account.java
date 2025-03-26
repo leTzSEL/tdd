2571fe70d39fb11a71c60fc3379c4978899cc880
letizia.enriquez <letizia4enriquez@gmail.com>
2025-03-26 08:08:59 +0100

    second and third TDD practice

--- src/main/java/lenriquez/example/tdd/Account.java
+++ src/main/java/lenriquez/example/tdd/Account.java
@@ -3,7 +3,12 @@ package lenriquez.example.tdd;
 public class Account {
     int balance = 0;
 
+
     public int totalBalance() {
         return balance;
+    }
+
+    public int increaseBalance(int amount) {
+        return balance += amount;
     }
 }
--- src/test/java/lenriquez/example/tdd/TestAccount.java
+++ src/test/java/lenriquez/example/tdd/TestAccount.java
@@ -9,7 +9,18 @@ public class TestAccount {
     @Test
     void newAccountBalance() {
         Account account1 = new Account();
-
         assertEquals(0, account1.totalBalance());
+    }
+
+    @Test
+    void newIncreaseBalance() {
+        Account account1 = new Account();
+        assertEquals(10,account1.increaseBalance(10));
+    }
+
+    @Test
+    void newIncreaseBalanceBig() {
+        Account account1 = new Account();
+        assertEquals(2000,account1.increaseBalance(2000));
     }
 }
